50c6ec720400c637ec15e9db08a21a08c0c25046
Helena Jakobsen <helenajakobsen@helenas-air-4.lan>
2021-01-07 13:57:02 +0100

    burger menu

--- script.js
+++ script.js
@@ -1,32 +1,8 @@
-window.addEventListener("load", sidenVises);
-
-function sidenVises() {
-    console.log("sidenVises");
-    document.querySelector("#knap").addEventListener("click", toggle);
-}
-
-function toggle() {
-    console.log("toggle");
-    document.querySelector("#toggle").classList.toggle("hidden");
-
-    let erSkjult = document.querySelector("#toggle").classList.contains("hidden");
-
-    if (erSkjult == true) {
-        document.querySelector("#knap").textContent = "☰";
-    } else {
-        document.querySelector("#knap").textContent = "X";
-    }
-}
-
-
-
-
-
-/*const knap = document.querySelector('#knap');
+const knap = document.querySelector('#knap');
 const box = document.querySelector('nav');
 
 knap.addEventListener('click', toggle);
 
 function toggle() {
     box.classList.toggle('toggle');
-}*/
+}
