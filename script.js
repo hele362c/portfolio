9e705847b51f6c11ea5030fc0d5c3f51d1110800
Helena Jakobsen <helenajakobsen@helenas-air-4.lan>
2021-01-10 16:29:04 +0100

    indhold script css

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ knap.addEventListener('click', toggle);
 function toggle() {
     box.classList.toggle('toggle');
 }
+
+/*slideshow for mockup*/
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    console.log("carousel");
+    var i;
+    var x = document.getElementsByClassName("mockup");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {
+        myIndex = 1
+    }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 2000);
+}
